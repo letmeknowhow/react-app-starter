c64dc5dbdd09ec8f29e209a368cd79bd797017d7
nxy <nxy801111@hotmail.com>
2016-06-26 17:49:51 +0800

    删除无效文件

--- server.js
+++ server.js
@@ -11,43 +11,6 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-//获取所有的栏目
-app.get('/wapindex.aspx', function (req, res) {
-    request.get({
-        url: 'http://xk-web.kelink.com/wapindex.aspx', qs: req.query
-    }, function (err, httpResponse, body) {
-        res.send(body);
-    });
-});
-
-//获取文章列表
-app.get('/article/list.aspx', function (req, res) {
-    request.get({
-        url: 'http://xk-web.kelink.com/article/list.aspx', qs: req.query
-    }, function (err, httpResponse, body) {
-        res.send(body);
-    });
-});
-
-//获取文章内容
-app.get('/article/view.aspx', function (req, res) {
-
-    request.get({
-        url: 'http://xk-web.kelink.com/article/view.aspx', qs: req.query
-    }, function (err, httpResponse, body) {
-        res.send(body);
-    });
-});
-
-//登录
-
-app.post('/waplogin.aspx', function (req, res) {
-    request.post({ url: 'http://xk-web.kelink.com/JsonAPI/waplogin.aspx', form: req.body }, function (err, httpResponse, body) {
-        res.send(body);
-    });
-
-});
-
 //设置任意路由都返回html
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/webapp.html')
--- start.bat
+++
@@ -1,6 +0,0 @@
-echo 启动服务器
-cd E:\react-kelink
-start npm start
-echo 编译前端代码
-cd E:\react-kelink
-start webpack -w
