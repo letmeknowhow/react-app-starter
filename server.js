18d6fcb8653efea74a27a7ab10040e71dc8dcfe7
nxy <nxy801111@hotmail.com>
2016-07-06 12:35:51 +0800

    hot load

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
-
-
-//设置静态文件目录
-app.use(express.static(__dirname + '/static'))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-//设置任意路由都返回html
-app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/webapp.html')
-});
-
-//创建服务器
-app.listen(3000, function () {
-    console.log('请在浏览器中打开：http://localhost:3000/')
-});
-
-//var webpack = require('webpack');
-//var WebpackDevServer = require('webpack-dev-server');
-//var config = require('./webpack.config');
+//var express = require('express');
+//var bodyParser = require('body-parser');
+//var request = require('request');
+//var app = express();
 //
-//new WebpackDevServer(webpack(config), {
-//    publicPath: config.output.publicPath,
-//    hot: true,
-//    historyApiFallback: true
-//}).listen(3000, 'localhost', function (err, result) {
-//    if (err) console.log(err);
-//    console.log('Listening at localhost:3000');
+//
+////设置静态文件目录
+//app.use(express.static(__dirname + '/static'))
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({
+//    extended: false
+//}));
+//
+////设置任意路由都返回html
+//app.get('*', function (req, res) {
+//    res.sendFile(__dirname + '/webapp.html')
 //});
+//
+////创建服务器
+//app.listen(3000, function () {
+//    console.log('请在浏览器中打开：http://localhost:3000/')
+//});
+
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Listening at http://localhost:3000/');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,41 +4,35 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
 
 module.exports = {
-  entry: {
-    app: './src/app', //编译的入口文件
-  },
-  output: {
-    publicPath: '/build/', //服务器根路径
-    path: './static/build', //编译到当前目录
-    filename: '[name].js' //编译后的文件名字
-  },
+  //entry: {
+  //  app: './src/app', //编译的入口文件
+  //},
+  //output: {
+  //  publicPath: '/build/', //服务器根路径
+  //  path: './static/build', //编译到当前目录
+  //  filename: '[name].js' //编译后的文件名字
+  //},
 
   /**
    * 支持热替换的配置
    */
-  //entry: [
-  //  'webpack-dev-server/client?http://localhost:3000',//资源服务器地址
-  //  'webpack/hot/only-dev-server',
-  //  './src/app'
-  //],
-  //output: {
-  //  publicPath: 'http://127.0.0.1:9090/static/build/',
-  //  path: './static/build', //编译到当前目录
-  //  filename: 'app.js' //编译后的文件名字
-  //},
-  //热替换
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/app'
+  ],
+  output: {
+    path: path.join(__dirname, 'build'), //编译到当前目录,
+    filename: 'app.js', //编译后的文件名字
+    publicPath: '/static/'
+  },
 
   module: {
     loaders: [
-      //{
-      //  test: /\.js?$/,
-      //  loaders: ['react-hot', 'babel'],
-      //  include: [path.join(__dirname, 'src')]
-      //},
       {
         test: /\.js$/,
         exclude: /^node_modules$/,
-        loader: 'babel?presets=es2015'
+        loaders: [ 'babel?presets=es2015']
       }, {
         test: /\.css$/,
         exclude: /^node_modules$/,
@@ -58,22 +52,21 @@ module.exports = {
       }, {
         test: /\.jsx$/,
         exclude: /^node_modules$/,
-        loaders: ['jsx', 'babel?presets[]=es2015,presets[]=react']
+        loaders: ['react-hot', 'jsx', 'babel?presets[]=es2015,presets[]=react']
       }
     ]
   },
   plugins: [
-    new webpack.DefinePlugin({ //编译成生产版本
-      //'process.env': {
-      //  NODE_ENV: JSON.stringify('production')
-      //}
-      //'process.env.NODE_ENV': '"production"'   //开发版本
-      'process.env.NODE_ENV': '"development"'   //开发版本
-    }),
-    new ExtractTextPlugin('[name].css'),
+    //new webpack.DefinePlugin({ //编译成生产版本
+    //  //'process.env': {
+    //  //  NODE_ENV: JSON.stringify('production')
+    //  //}
+    //  //'process.env.NODE_ENV': '"production"'   //开发版本
+    //  'process.env.NODE_ENV': '"development"'   //开发版本
+    //}),
+    new ExtractTextPlugin('app.css'),
     //commonsPlugin,
-    //new webpack.HotModuleReplacementPlugin(),
-    //new webpack.NoErrorsPlugin()
+    new webpack.HotModuleReplacementPlugin()
   ],
   resolve: {
     extensions: ['', '.js', '.jsx'], //后缀名自动补全
