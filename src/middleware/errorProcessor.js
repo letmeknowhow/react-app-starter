65315a4143e7dd5e43f7e60b699b6b1b6bc22cd1
NiuXiaoYu(营销事业部_技术部) <niuxiaoyu@sogou-inc.com>
2018-06-08 20:28:18 +0800

    123123123

--- src/middleware/errorProcessor.js
+++ src/middleware/errorProcessor.js
@@ -6,9 +6,9 @@
 import { ACTION_TYPE_ADD_ERROR } from 'common/constant';
 
 export default ({ dispatch, getState }) => next => action => {
-  if(action.res && action.res.flag == '1'){
+  if (action.res && action.res.flag == '1') {
     dispatch({ type: ACTION_TYPE_ADD_ERROR, payload: { errorMsg: action.res.msg[0] } });
-  } else if(action.payload && action.payload.flag == '1'){
+  } else if (action.payload && action.payload.flag == '1') {
     dispatch({ type: ACTION_TYPE_ADD_ERROR, payload: { errorMsg: action.payload.msg[0] } });
   }
   return next(action);
